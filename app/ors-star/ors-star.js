4db7a622af25fe705ea897d3198495dd65156d78
Jean-Louis GUENEGO <jlguenego@gmail.com>
2016-06-27 16:56:28 +0200

    ng-cloak

--- app/ors-star/ors-star.js
+++ app/ors-star/ors-star.js
@@ -6,8 +6,16 @@
 	app.directive('orsStar', function() {
 		return {
 			restrict: 'CEA',
-			link: function() {
+			link: function(scope, element, attrs) {
 				console.log('link', arguments);
+				var html = '';
+				for (var i = 0; i < 3; i++) {
+					html += '<img src="ors-star/img/yellow_star.png" />';
+				}
+				for (var i = 3; i < 5; i++) {
+					html += '<img src="ors-star/img/white_star.png" />';
+				}
+				element.html(html);
 			}
 		};
 	});
