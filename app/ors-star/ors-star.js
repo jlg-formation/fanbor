becacef65874dd6016b5fa83b222e22835337cb5
Jean-Louis GUENEGO <jlguenego@gmail.com>
2016-06-28 10:58:53 +0200

    note attr

--- app/ors-star/ors-star.js
+++ app/ors-star/ors-star.js
@@ -8,11 +8,12 @@
 			restrict: 'CEA',
 			link: function(scope, element, attrs) {
 				console.log('link', arguments);
+				var note = attrs.note || 3;
 				var html = '';
-				for (var i = 0; i < 3; i++) {
+				for (var i = 0; i < note; i++) {
 					html += '<img src="ors-star/img/yellow_star.png" />';
 				}
-				for (var i = 3; i < 5; i++) {
+				for (var i = note; i < 5; i++) {
 					html += '<img src="ors-star/img/white_star.png" />';
 				}
 				element.html(html);
